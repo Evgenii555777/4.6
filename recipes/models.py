afd2a41c77eb3f679732070ead613f2862ac0205
Evgenii555777 <9626863746@mail.ru>
2023-09-28 19:17:54 +0300

    Committing all files

---
+++ recipes/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
