b8942e645f8869046ce2ba9387998ac8375cb166
Evgenii555777 <9626863746@mail.ru>
2023-09-29 11:28:27 +0300

    v2

--- src/App.js
+++ src/App.js
@@ -5,16 +5,11 @@ import RecipeList from './RecipeList';
 import RecipeDetail from './RecipeDetail';
 import CategoryDetail from './CategoryDetail';
 
-function YourHomePageComponent() {
-  return <div>Home Page Content</div>;
-}
-
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<YourHomePageComponent />} />
-        <Route path="/categories" element={<CategoryList />} />
+        <Route path="/" element={<CategoryList />} />
         <Route path="/category/:id" element={<CategoryDetail />} />
         <Route path="/recipe" element={<RecipeList />} />
         <Route path="/recipe/:id" element={<RecipeDetail />} />
